5c3be54958fa250cbe253ad06c615c599531e96b
Himank Pathak <himankpathak@gmail.com>
2018-12-14 22:57:58 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 4096 Game
+a rip-off of 2048 game made in python3 which can be played on terminal
+
---
+++ py4096.py
@@ -0,0 +1,23 @@
+
+
+if __name__ == "__main__":
+  	
+	print"""
+	Welcome to 4096 v0.1
+
+	Objective: Make The 4096 Tile To Win!!
+
+	Instructions :
+	1. Turn On Your Num Lock.
+	2. Use Following Keys To Navigate:
+
+				8 - Up
+		4 - Left        6 - Right
+				2 - Down
+				
+	3. You can also use WASD.
+				w - Up
+		a - Left        d - Right
+				s - Down
+
+	"""
